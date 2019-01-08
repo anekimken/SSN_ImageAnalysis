56d50f571cb45ede0a1bf9f9750d16ecc2b6943e
Adam Nekimken <adam.nekimken@gmail.com>
2019-01-07 16:44:32 -0800

    Starting StrainPropogationTrial class

---
+++ StrainPropagationTrial.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan  7 15:24:53 2019
+
+@author: Adam Nekimken
+"""
+
+class StrainPropagationTrial:
+    def __init__(self,filename):
+        self.filename = filename
