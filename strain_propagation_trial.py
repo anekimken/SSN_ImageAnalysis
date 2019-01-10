d07b9711e3313236d833932a0121b6ba3bde56c6
Adam Nekimken <adam.nekimken@gmail.com>
2019-01-09 18:28:27 -0800

    Removed pandas module from metadata retrieval
    Not necessary. Doing it all with dictionaries now

--- strain_propagation_trial.py
+++ strain_propagation_trial.py
@@ -11,9 +11,9 @@ import os
 import pathlib
 import datetime
 import warnings
+from typing import Tuple
 
 import numpy as np
-import pandas as pd
 import pims
 from PIL import Image
 from nd2reader import ND2Reader
@@ -31,7 +31,7 @@ warnings.filterwarnings("ignore",
 warnings.filterwarnings("ignore", message="Reported average frame interval")
 
 
-class StrainPropagationTrial:
+class StrainPropagationTrial(object):
     """Class for analyzing one trial of strain propagation experiment
 
     Loads previously analyzed data, by default loads images, by default
@@ -54,7 +54,10 @@ class StrainPropagationTrial:
         image_array: Numpy array with image data. Can be null if not loaded
 
     """
-    def __init__(self, filename, load_images=True, overwrite_metadata=False):
+    def __init__(self,
+                 filename: str,
+                 load_images: bool = True,
+                 overwrite_metadata: bool = False):
         self.filename = filename
         self.load_images = load_images
         self.overwrite_metadata = overwrite_metadata
@@ -77,9 +80,10 @@ class StrainPropagationTrial:
         if not self.analyzed_data_location.is_dir():
             self.analyzed_data_location.mkdir()
 
-        self.load_trial()  # This function is what makes the init slow
+        # This function is what makes the init slow
+        self.image_array, self.metadata = self.load_trial()
 
-    def load_trial(self):
+    def load_trial(self) -> Tuple[np.array, dict]:
         """Loads the data and metadata for this trial from disk.
 
         Loads the data needed for analyzing the trial. Also handles
@@ -91,25 +95,34 @@ class StrainPropagationTrial:
         large amount of data, so the goal here is to load as little data
         as possible.
 
-        """
+        Returns:
+            image_array (np.array): Image data. Can be empty if not loaded
+            metadata (dict): All the metadata
 
+        """
+        # TODO: Load other analyzed data here too
         if (self.load_images is False and  # don't load images
                 self.overwrite_metadata is False and  # don't overwrite
                 self.metadata_file_path.is_file() is True):  # yaml exists
-            # only load metadata in this condition
-            self.metadata = self._load_metadata_from_yaml()
+            # only load metadata in this case
+            metadata = self._load_metadata_from_yaml()
+            # TODO: create empty numpy array of the right size
+            image_array = np.array([2, 2, 2, 2])
+
         else:
-            self.image_array, images = self._load_images_from_disk()
+            image_array, images = self._load_images_from_disk()
             # If we don't have a yaml metadata file yet, we need to get
             # some information from Google Drive and build a yaml file
             if (self.metadata_file_path.is_file() is False or  # no yaml yet
                     self.overwrite_metadata is True):
-                self.metadata = self._retrieve_metadata(images)
-                self._write_metadata_to_yaml(self.metadata)
+                metadata = self._retrieve_metadata(images)
+                self._write_metadata_to_yaml(metadata)
             else:
                 # if we have metadata in a yaml file, no need to go to
                 # Google Drive to get it. Just load from yaml
-                self.metadata = self._load_metadata_from_yaml()
+                metadata = self._load_metadata_from_yaml()
+
+        return image_array, metadata
 
     def test_parameters(self):
             pass
@@ -117,99 +130,99 @@ class StrainPropagationTrial:
     def run_batch(self):
             pass
 
-    def _load_images_from_disk(self):
+    def _load_images_from_disk(self) -> Tuple[np.array, ND2Reader]:
         """Accesses the image data from the file."""
         images = pims.open(self.filename)
-        print(type(images))
         images.bundle_axes = ['z', 'y', 'x']
         image_array = np.asarray(images)
         image_array = image_array.squeeze()
 
         return image_array, images
 
-    def _load_metadata_from_yaml(self):
+    def _load_metadata_from_yaml(self) -> dict:
         """Loads metadata from an existing yaml file."""
         with open(self.metadata_file_path, 'r') as yamlfile:
             metadata = yaml.load(yamlfile)
 
         return metadata
 
-    def _write_metadata_to_yaml(self, metadata_dict):
+    def _write_metadata_to_yaml(self, metadata_dict: dict):
         """Takes metadata dict and writes it to a yaml file"""
         with open(self.metadata_file_path, 'w') as output_file:
                 yaml.dump(metadata_dict, output_file,  # create file
                           explicit_start=True, default_flow_style=False)
 
-    def _retrieve_metadata(self, images):
+    def _retrieve_metadata(self, images: ND2Reader) -> dict:
         """Retrieves metadata from Google Drive and from the image file.
 
         Args:
-            images (nd2reader object): Images for this trial
+            images (ND2Reader): Reader object containing images for this trial
 
         """
-
         # Access google drive spreadsheet
         scope = ['https://spreadsheets.google.com/feeds',
                  'https://www.googleapis.com/auth/drive']
         keyfile = 'SenseOfTouchResearch-e5927f56c4d0.json'
         credentials = ServiceAccountCredentials.from_json_keyfile_name(
                 keyfile, scope)
-        c = gspread.authorize(credentials)
-        metadataSpread = c.open_by_key(
+        gspread_client = gspread.authorize(credentials)
+        metadata_spread = gspread_client.open_by_key(
                 '1LsTdPBOW79XSkk5DJv2ckiVTvpofAOOd_dJL4cgtxBQ')
-        metadataFrame = pd.DataFrame(
-                metadataSpread.sheet1.get_all_records())
-        gdriveMetadata = metadataFrame.loc[metadataFrame[(
-                'Experiment ID')] == self.experiment_id]
+        metadata_worksheet = metadata_spread.worksheet("MetadataResponses")
+        current_id_cell = metadata_worksheet.find(self.experiment_id)
+        row_of_metadata = metadata_worksheet.row_values(current_id_cell.row)
+        row_of_keys = metadata_worksheet.row_values(1)
+        gdrive_metadata_dict = dict(zip(row_of_keys, row_of_metadata))
 
         # Access the metadata from the file
+        # TODO: add support for number of z_levels
         meta = images.metadata
-        keysToKeep = ['height', 'width',
-                      'date', 'total_images_per_channel',
-                      'channels', 'pixel_microns']
-        metadataFromScope = {key: meta[key] for key in keysToKeep}
-        metadataFromScope = pd.DataFrame(metadataFromScope)
+        keys_to_keep = ['height', 'width',
+                        'date', 'total_images_per_channel',  # 'z_levels'
+                        'channels', 'pixel_microns', 'num_frames']
+        metadata_from_scope = {key: (meta[key]) for key in keys_to_keep}
 
-        # Combine metadata from the two sources to get all of it together
-        gdriveMetadata = gdriveMetadata.reset_index(drop=True)
-        metadataFromScope = metadataFromScope.reset_index(drop=True)
-        retrieved_metadata = metadataFromScope.join(gdriveMetadata)
+        # Combine metadata from both sources into one dictionary
+        combined_metadata = {**gdrive_metadata_dict, **metadata_from_scope}
 
-        # Format metadata into a big dictionary for writing to yaml
-        time_str = retrieved_metadata.iloc[0]['Timestamp']
-        bleach_date = retrieved_metadata.iloc[0]['Bleach Date']
-        bleach_time = retrieved_metadata.iloc[0]['Bleach Time']
+        time_str = combined_metadata['Timestamp']
+        bleach_date = combined_metadata['Bleach Date']
+        bleach_time = combined_metadata['Bleach Time']
         metadata_dict = {
-                'experiment_id': self.experiment_id,
-                'slice_height_pix': int(retrieved_metadata.iloc[0]['height']),
-                'slice_width_pix': int(retrieved_metadata.iloc[0]['width']),
+                'Experiment_id': self.experiment_id,
+                'slice_height_pix': int(combined_metadata['height']),
+                'slice_width_pix': int(combined_metadata['width']),
+                # 'stack_height': int(
+                #         combined_metadata['z_levels'][-1]),
+                'num_timepoints': int(
+                        combined_metadata['num_frames']),
                 'timestamp': datetime.datetime.strptime(
                         time_str, '%m/%d/%Y %H:%M:%S'),
-                'total_images': int(retrieved_metadata.iloc[0][
+                'total_images': int(combined_metadata[
                         'total_images_per_channel']),
-                'microscope_channel': retrieved_metadata.iloc[0]['channels'],
-                'pixel_microns': float(retrieved_metadata.iloc[0][
+                'microscope_channel': combined_metadata['channels'][0],
+                'pixel_microns': float(combined_metadata[
                         'pixel_microns']),
                 'bleach_time': datetime.datetime.strptime(
                         bleach_date + ' ' + bleach_time,
                         '%m/%d/%Y %H:%M:%S %p'),
-                'cultivation_temp': retrieved_metadata.iloc[0][
-                        'Cultivation Temperature (°C)'],
-                'device_ID': retrieved_metadata.iloc[0]['Device ID'],
-                'user': retrieved_metadata.iloc[0]['Email Address'],
-                'worm_life_stage': retrieved_metadata.iloc[0]['Life stage'],
-                'microscope': retrieved_metadata.iloc[0]['Microscope'],
-                'neuron': retrieved_metadata.iloc[0]['Neuron'],
-                'notes': retrieved_metadata.iloc[0]['Notes'],
-                'num_eggs': retrieved_metadata.iloc[0][
-                        'Number of eggs visible'],
-                'microscope_objective': retrieved_metadata.iloc[0][
+                'cultivation_temp': int(combined_metadata[
+                        'Cultivation Temperature (°C)']),
+                'device_ID': combined_metadata['Device ID'],
+                'user': combined_metadata['Email Address'],
+                'worm_life_stage': combined_metadata['Life stage'],
+                'microscope': combined_metadata['Microscope'],
+                'neuron': combined_metadata['Neuron'],
+                'notes': combined_metadata['Notes'],
+                'num_eggs': int(combined_metadata[
+                        'Number of eggs visible']),
+                'microscope_objective': combined_metadata[
                         'Objective'],
-                'purpose': retrieved_metadata.iloc[0]['Purpose of Experiment'],
-                'worm_strain': retrieved_metadata.iloc[0]['Worm Strain'],
-                'head_orientation': retrieved_metadata.iloc[0][
+                'purpose': combined_metadata['Purpose of Experiment'],
+                'worm_strain': combined_metadata['Worm Strain'],
+                'head_orientation': combined_metadata[
                         'Worm head orientation'],
-                'vulva_orientation': retrieved_metadata.iloc[0][
+                'vulva_orientation': combined_metadata[
                         'Worm vulva orientation']}
 
         return metadata_dict
@@ -224,5 +237,6 @@ if __name__ == '__main__':
     test_trial = StrainPropagationTrial(
             '/Users/adam/Documents/''SenseOfTouchResearch/'
             'SSN_data/20181220/SSN_126_001.nd2',
-            load_images=True, overwrite_metadata=False)
+            load_images=False, overwrite_metadata=True)
     my_metadata = test_trial.metadata
+    my_image = test_trial.image_array
