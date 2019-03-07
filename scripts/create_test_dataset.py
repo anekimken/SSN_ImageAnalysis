07ed27e9c5caf0cec4e5a2ba3f1b10b6377c4641
Adam Nekimken <adam.nekimken@gmail.com>
2019-03-07 15:58:51 -0800

    Implemented testing, change strain calculation function to use dataframe
    strain calc is much cleaner now. still need to do some testing with it.

--- scripts/create_test_dataset.py
+++ scripts/create_test_dataset.py
@@ -5,86 +5,89 @@ Created on Fri Mar  1 16:17:57 2019
 
 @author: adam
 
-Creates and analyzes a 'trial' where the results are known
+Creates an image where the locations of "mitochondria" are known
 """
 
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.stats import multivariate_normal
 
-# Create image background using gaussian-distributed noise
-mu, sigma = 100, 2.2  # mean and standard deviation
-background = np.random.normal(mu, sigma, (65, 1200, 600))
 
-# Create 3d gaussian kernel to represent mitochondria
-xy_rad = 15
-z_rad = 21
-x_values = np.linspace(-xy_rad, xy_rad, xy_rad * 2 + 1)
-y_values = np.linspace(-xy_rad, xy_rad, xy_rad * 2 + 1)
-z_values = np.linspace(-z_rad, z_rad, z_rad * 2 + 1)
-x, y, z = np.mgrid[-xy_rad:xy_rad + 1:1, -xy_rad:xy_rad + 1:1,
-                   -z_rad:z_rad+1:1]
-pos = np.empty(x.shape + (3,))
-pos[:, :, :, 0] = x
-pos[:, :, :, 1] = y
-pos[:, :, :, 2] = z
+def create_test_stack(mito_coords, size,  show_mito=False):
+    # Create image background using gaussian-distributed noise
+    mu, sigma = 100, 2.2  # mean and standard deviation
+    image = np.random.normal(mu, sigma, size)
 
-rv = multivariate_normal.pdf(pos, mean=[0, 0, 0],
-                             cov=[[xy_rad * 2, 0, 0],
-                                  [0, xy_rad, 0],
-                                  [0, 0, z_rad]])
-multiplier = 200 / rv.max()
-mito = multiplier * np.transpose(rv, (2, 0, 1))  # microscope convention
+    # Create 3d gaussian kernel to represent mitochondria
+    xy_rad = 9
+    z_rad = 13
+    x_values = np.linspace(-xy_rad, xy_rad, xy_rad * 2 + 1)
+    y_values = np.linspace(-xy_rad, xy_rad, xy_rad * 2 + 1)
+    z_values = np.linspace(-z_rad, z_rad, z_rad * 2 + 1)
+    x, y, z = np.mgrid[-xy_rad:xy_rad + 1:1, -xy_rad:xy_rad + 1:1,
+                       -z_rad:z_rad+1:1]
+    pos = np.empty(x.shape + (3,))
+    pos[:, :, :, 0] = x
+    pos[:, :, :, 1] = y
+    pos[:, :, :, 2] = z
 
-# Show plot of mitochondria from a couple angles just to be sure
-max_proj = np.amax(mito, 2)  # collapse y axis
-max_proj = max_proj.squeeze()
-xz_fig, xz_ax = plt.subplots(figsize=(z_rad / 2, xy_rad / 2))
-xz_ax.contourf(x_values, z_values, max_proj)
-xz_ax.set_xlabel('z coord on microscope')
-xz_ax.set_ylabel('x coord on microscope')
+    rv = multivariate_normal.pdf(pos, mean=[0, 0, 0],
+                                 cov=[[xy_rad, 0, 0],
+                                      [0, xy_rad, 0],
+                                      [0, 0, z_rad]])
+    multiplier = 200 / rv.max()
+    mito = multiplier * np.transpose(rv, (2, 0, 1))  # microscope convention
 
-max_proj = np.amax(mito, 0)  # collapse z axis
-max_proj = max_proj.squeeze()
-xy_fig, xy_ax = plt.subplots(figsize=(4, 4))
-xy_ax.contourf(x_values, y_values, max_proj)
-xy_ax.set_xlabel('x coord on microscope')
-xy_ax.set_ylabel('y coord on microscope')
+    # Show plot of mitochondria from a couple angles just to be sure
+    if show_mito is True:
+        max_proj = np.amax(mito, 2)  # collapse y axis
+        max_proj = max_proj.squeeze()
+        xz_fig, xz_ax = plt.subplots(figsize=(z_rad / 2, xy_rad / 2))
+        xz_ax.contourf(x_values, z_values, max_proj)
+        xz_ax.set_xlabel('z coord on microscope')
+        xz_ax.set_ylabel('x coord on microscope')
 
+        max_proj = np.amax(mito, 0)  # collapse z axis
+        max_proj = max_proj.squeeze()
+        xy_fig, xy_ax = plt.subplots(figsize=(4, 4))
+        xy_ax.contourf(x_values, y_values, max_proj)
+        xy_ax.set_xlabel('x coord on microscope')
+        xy_ax.set_ylabel('y coord on microscope')
 
-def add_mito(image, coords, fake_mito):
-    x_start = coords[1] - int(fake_mito.shape[1] / 2)
-    x_end = coords[1] + int(fake_mito.shape[1] / 2 + 1)
-    y_start = coords[2] - int(fake_mito.shape[2] / 2)
-    y_end = coords[2] + int(fake_mito.shape[2] / 2 + 1)
-    z_start = coords[0] - int(fake_mito.shape[0] / 2)
-    z_end = coords[0] + int(fake_mito.shape[0] / 2 + 1)
+    def add_mito(image, coords, fake_mito):
+        frame = coords['frame']
+        x_start = coords['x'] - int(fake_mito.shape[2] / 2)
+        x_end = coords['x'] + int(fake_mito.shape[2] / 2 + 1)
+        y_start = coords['y'] - int(fake_mito.shape[1] / 2)
+        y_end = coords['y'] + int(fake_mito.shape[1] / 2 + 1)
+        z_start = coords['z'] - int(fake_mito.shape[0] / 2)
+        z_end = coords['z'] + int(fake_mito.shape[0] / 2 + 1)
 
-    image[z_start:z_end, x_start:x_end, y_start:y_end] = fake_mito[:, :, :]
+        image[frame, z_start:z_end,
+              y_start:y_end, x_start:x_end] = fake_mito[:, :, :]
+
+        return image
+
+    # Add mitochondria to the image
+    for i in range(len(mito_coords)):
+        image = add_mito(image,
+                         mito_coords.iloc[i][['z', 'y', 'x', 'frame']],
+                         mito)
 
     return image
 
 
-# Add mitochondria to the image
-image = background.copy()
-mito1_coords = [32, 500, 350]
-mito2_coords = [30, 259, 320]
-image = add_mito(image, mito1_coords, mito)
-image = add_mito(image, mito2_coords, mito)
-
-max_proj = np.amax(image, 0)  # collapse z axis
-max_proj = max_proj.squeeze()
-im_fig, im_ax = plt.subplots(figsize=(8, 16))
-im_ax.imshow(max_proj, interpolation='none')
-im_ax.set_xlabel('x coord on microscope')
-im_ax.set_ylabel('y coord on microscope')
-
-# Run analysis
-
-# Save to results file format
-# metadata.yaml
-# trackpyBatchParams.yaml
-# trackpyBatchParamsHistory.yaml
-# trackpyBatchResults.yaml
-# trackpyParamTestHistory.yaml
-# unlinkedTrackpyBatchResults.yaml
+if __name__ == '__main__':
+    mito_coords = pd.DataFrame([[30, 200, 350, 0],
+                                [30, 250, 350, 0],
+                                [30, 300, 350, 0],
+                                [30, 350, 350, 0]],
+                               columns=['z', 'y', 'x', 'frame'])
+    image = create_test_stack(mito_coords, (1, 61, 1200, 600))
+    max_proj = np.amax(image[0], 0)  # collapse z axis
+    max_proj = max_proj.squeeze()
+    im_fig, im_ax = plt.subplots(figsize=(8, 16))
+    im_ax.imshow(max_proj, interpolation='none')
+    im_ax.set_xlabel('x coord on microscope')
+    im_ax.set_ylabel('y coord on microscope')
---
+++ test/test_Strain_PropagationTrial.py
@@ -0,0 +1,277 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan  7 17:01:49 2019
+
+@author: Adam
+"""
+
+# TODO: DOCSTRINGS!!!!
+import unittest
+import datetime
+import numpy as np
+import pandas as pd
+import pathlib
+
+from strain_propagation_trial import StrainPropagationTrial
+import scripts.create_test_dataset as create_test_dataset
+
+
+@unittest.skip("skipping load_trial tests")
+class TestLoadTrial(unittest.TestCase):
+    def setUp(self):
+        self.filename = ('/Users/adam/Documents/SenseOfTouchResearch/'
+                         'SSN_data/20181220/SSN_126_001.nd2')
+        self.test_cases = [(False, False)]
+#                           (False, True)]
+
+    def test_load_trial(self):
+        for load_image_flag, overwrite_metadata_flag in self.test_cases:
+            print("Checking case :", load_image_flag,
+                  overwrite_metadata_flag)
+            trial = StrainPropagationTrial()
+            trial.load_trial(
+                    self.filename,
+                    load_images=load_image_flag,
+                    overwrite_metadata=overwrite_metadata_flag)
+
+            self.assertIsInstance(trial.metadata, dict)
+            self.assertEqual(trial.metadata["Experiment_id"],
+                             "SSN_126_001")
+            bleach_datetime = datetime.datetime(2018, 12, 17, 9, 30, 0)
+            self.assertEqual(trial.metadata["bleach_time"],
+                             bleach_datetime)
+            self.assertEqual(trial.metadata["cultivation_temp"], 20)
+            self.assertEqual(trial.metadata["device_ID"], 'Y4P7-7')
+            self.assertEqual(trial.metadata["head_orientation"],
+                             'Headfirst')
+            self.assertEqual(trial.metadata["microscope"],
+                             'Spinning Disk Confocal in CSIF Shriram')
+            self.assertEqual(trial.metadata["microscope_channel"],
+                             'SDC 561-ALN')
+            self.assertEqual(trial.metadata["microscope_objective"],
+                             '40x oil')
+            self.assertEqual(trial.metadata["neuron"], 'PVM')
+            self.assertEqual(trial.metadata["notes"],
+                             'Neuron not all in one plane. Bleached after '
+                             'a few trials, especially mitochondria that '
+                             'were deeper into the worm. 11 total stacks, '
+                             '4 with actuation: 0, 0, 300, 0, 300, 0, 300,'
+                             ' 0, 300, 0, 0 kPa.')
+            self.assertEqual(trial.metadata["num_eggs"], 3)
+            self.assertEqual(trial.metadata["pixel_microns"], 0.275)
+            self.assertEqual(trial.metadata["purpose"],
+                             'Baseline measurement')
+            self.assertEqual(trial.metadata["slice_height_pix"], 1200)
+            self.assertEqual(trial.metadata["slice_width_pix"], 600)
+            timestamp_datetime = datetime.datetime(2018, 12, 20, 11, 5, 37)
+            self.assertEqual(trial.metadata["timestamp"],
+                             timestamp_datetime)
+            self.assertEqual(trial.metadata["total_images"], 759)
+            self.assertEqual(trial.metadata["user"],
+                             'anekimke@stanford.edu')
+            self.assertEqual(trial.metadata["vulva_orientation"], 'West')
+            self.assertEqual(trial.metadata["worm_life_stage"], 'YA')
+            self.assertEqual(trial.metadata["worm_strain"], 'NM3573')
+
+
+@unittest.skip("skipping run_batch tests")
+class TestFindMitosArtificialData(unittest.TestCase):
+    def setUp(self):
+        self.mito_coords = pd.DataFrame([[30, 200, 350, 0],
+                                         [30, 250, 350, 0],
+                                         [30, 300, 350, 0],
+                                         [30, 350, 350, 0],
+                                         [30, 200, 350, 1],
+                                         [30, 250, 360, 1],
+                                         [30, 300, 360, 1],
+                                         [30, 350, 350, 1],
+                                         [30, 200, 350, 2],
+                                         [30, 250, 350, 2],
+                                         [30, 300, 350, 2],
+                                         [30, 350, 350, 2]],
+                                        columns=['z', 'y', 'x', 'frame'])
+        self.stack_size = (3, 61, 1200, 600)
+        self.images = np.ndarray(self.stack_size)
+        self.images = create_test_dataset.create_test_stack(
+                    self.mito_coords, size=self.stack_size)
+        self.trial = StrainPropagationTrial()
+        self.trial.metadata = {'pressure_kPa': [0, 300, 0]}
+        self.trial.analyzed_data_location = pathlib.Path('./results_data')
+        data_loc = self.trial.analyzed_data_location
+        self.trial.batch_history_file = data_loc.joinpath(
+                'trackpyBatchParamsHistory.yaml')
+        # TODO: make test case contain all relevant info
+        self.test_cases = [{'images_ndarray': self.images,
+                            'roi': [300, 100, 400, 450],
+                            'gaussian_width': 3,
+                            'particle_z_diameter': 21,
+                            'particle_xy_diameter': 15,
+                            'brightness_percentile': 50,
+                            'min_particle_mass': 50,
+                            'bottom_slice': 0,
+                            'top_slice': 61,
+                            'tracking_seach_radius': 20,
+                            'last_timepoint': 3,
+                            'notes': 'Artificial data test'},
+                           {'images_ndarray': self.images,
+                            'roi': [200, 100, 500, 450],
+                            'gaussian_width': 3,
+                            'particle_z_diameter': 21,
+                            'particle_xy_diameter': 15,
+                            'brightness_percentile': 50,
+                            'min_particle_mass': 50,
+                            'bottom_slice': 0,
+                            'top_slice': 61,
+                            'tracking_seach_radius': 20,
+                            'last_timepoint': 3,
+                            'notes': 'Artificial data test'}]
+
+    def test_batch_find_entire_trial(self):
+        for case in self.test_cases:
+            self.trial.run_batch(**case)
+            linked = self.trial.linked_mitos.reset_index(drop=True)
+            pd.testing.assert_frame_equal(linked[['x', 'y', 'z', 'frame']],
+                                          self.mito_coords,
+                                          check_less_precise=True,
+                                          check_like=True,
+                                          check_dtype=False)
+
+
+class TestCalcStrainArtificialData(unittest.TestCase):
+    def setUp(self):
+        self.trial = StrainPropagationTrial()
+        self.trial.analyzed_data_location = pathlib.Path('./results_data')
+
+        self.test_cases = [
+                [pd.DataFrame([[30, 200, 350, 0, 0],
+                               [30, 250, 350, 0, 1],
+                               [30, 300, 350, 0, 2],
+                               [30, 350, 350, 0, 3],
+                               [30, 200, 350, 1, 0],
+                               [30, 250, 350, 1, 1],
+                               [30, 300, 350, 1, 2],
+                               [30, 350, 350, 1, 3],
+                               [30, 200, 350, 2, 0],
+                               [30, 250, 350, 2, 1],
+                               [30, 300, 350, 2, 2],
+                               [30, 350, 350, 2, 3]],
+                              columns=['z', 'y', 'x', 'frame', 'particle']),
+                 {'pressure_kPa': [0, 300, 0]}, [[0, 0, 0],
+                                                 [0, 0, 0],
+                                                 [0, 0, 0]]],
+
+                [pd.DataFrame([[30, 200, 350, 0, 0],
+                               [30, 250, 350, 0, 1],
+                               [30, 300, 350, 0, 2],
+                               [30, 350, 350, 0, 3],
+                               [30, 200, 350, 1, 0],
+                               [30, 250, 360, 1, 1],
+                               [30, 300, 360, 1, 2],
+                               [30, 350, 350, 1, 3],
+                               [30, 200, 350, 2, 0],
+                               [30, 250, 350, 2, 1],
+                               [30, 300, 350, 2, 2],
+                               [30, 350, 350, 2, 3]],
+                              columns=['z', 'y', 'x', 'frame', 'particle']),
+                 {'pressure_kPa': [0, 300, 0]}, [[0, 0, 0],
+                                                 [0.0198039, 0, 0.0198039],
+                                                 [0, 0, 0]]],
+
+                [pd.DataFrame([[30, 200, 350, 0, 0],
+                               [30, 250, 350, 0, 1],
+                               [30, 300, 350, 0, 2],
+                               [30, 350, 350, 0, 3],
+                               [30, 200, 350, 1, 0],
+                               [30, 260, 350, 1, 1],
+                               [30, 300, 350, 1, 2],
+                               [30, 350, 350, 1, 3],
+                               [30, 200, 350, 2, 0],
+                               [30, 250, 350, 2, 1],
+                               [30, 300, 350, 2, 2],
+                               [30, 350, 350, 2, 3]],
+                              columns=['z', 'y', 'x', 'frame', 'particle']),
+                 {'pressure_kPa': [0, 300, 0]}, [[0, 0, 0],
+                                                 [0.2, -0.2, 0],
+                                                 [0, 0, 0]]],
+                [pd.DataFrame([[30, 200, 350, 0, 0],
+                               [30, 250, 350, 0, 1],
+                               [30, 300, 350, 0, 2],
+                               [30, 350, 350, 0, 3],
+                               [30, 200, 350, 1, 0],
+                               [20, 250, 350, 1, 1],
+                               [20, 300, 350, 1, 2],
+                               [30, 350, 350, 1, 3],
+                               [30, 200, 350, 2, 0],
+                               [30, 250, 350, 2, 1],
+                               [30, 300, 350, 2, 2],
+                               [30, 350, 350, 2, 3]],
+                              columns=['z', 'y', 'x', 'frame', 'particle']),
+                 {'pressure_kPa': [0, 300, 0]}, [[0, 0, 0],
+                                                 [0.0198039, 0, 0.0198039],
+                                                 [0, 0, 0]]]]
+#                [pd.DataFrame([[30, 200, 350, 0, 0],
+#                               [27, 204, 350, 0, 1],
+#                               [30, 200, 350, 1, 0],
+#                               [33, 204, 350, 1, 1],
+#                               [30, 200, 350, 2, 0],
+#                               [33, 204, 350, 2, 1]],
+#                              columns=['z', 'y', 'x', 'frame', 'particle']),
+#                 {'pressure_kPa': [0, 300, 0]}, [[0.25],
+#                                                 [-.25],
+#                                                 [0.25]]]] # turns out this is just rotation, not deformation
+#            [pd.DataFrame([[30, 200, 350, 0, 0],
+#                               [30, 250, 350, 0, 1],
+#                               [30, 300, 350, 0, 2],
+#                               [30, 350, 350, 0, 3],
+#                               [30, 200, 350, 1, 0],
+#                               [20, 250, 350, 1, 1],
+#                               [20, 300, 350, 1, 2],
+#                               [30, 350, 350, 1, 3],
+#                               [30, 200, 350, 2, 0],
+#                               [20, 250, 350, 2, 1],
+#                               [20, 300, 350, 2, 2],
+#                               [30, 350, 350, 2, 3]],
+#                              columns=['z', 'y', 'x', 'frame', 'particle']),
+#                 {'pressure_kPa': [0, 300, 0]}, [[0.1, 0, 0.1],
+#                                                 [0.1, 0, 0.1],
+#                                                 [0.1, 0, 0.1]]]
+
+    def test_strain_calc_with_artificial_data(self):
+        for linked_mitos, pressure, desired_output in self.test_cases:
+            self.trial.linked_mitos = linked_mitos
+            self.trial.metadata = pressure
+            self.trial.calculate_strain()
+            np.testing.assert_almost_equal(self.trial.strain, desired_output)
+
+
+"""
+Things to test:
+    * Asterisk indicates highest priorities
+
+    Using actual data:
+        load_trial using actual nd2 data and actual metadata
+
+    Using artificial data:
+        find_mitos_one_stack
+        #*find_mitos_current_trial
+        link_existing_particles
+        *calculate_strain -> bug
+
+    Using a two sets of artificial data:
+        run_multiple_files this might take too long
+        add_trial_to_queue
+        run_queue/run_queue_item
+        remove_from_review_queue
+
+    Results files from artificial data:
+        metadata.yaml
+        trackpyBatchParams.yaml
+        *trackpyBatchResults.yaml
+        unlinkedTrackpyBatchResults.yaml
+        *strain.yaml
+
+"""
+
+if __name__ == '__main__':
+    unittest.main()
