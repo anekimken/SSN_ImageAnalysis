c6242c908bd32113ca1bd81d1e8dfc0485b0d6a8
Adam Nekimken <adam.nekimken@gmail.com>
2019-03-18 17:29:36 -0700

    Started implementing brightfield analysis tab
    For finding actuator location to make plotting work better

--- scripts/add_rating_to_metadata.py
+++ scripts/add_rating_to_metadata.py
@@ -35,21 +35,24 @@ for file in metadata_files:
     print(file)
     with open(file, 'r') as yamlfile:
         metadata = yaml.load(yamlfile)
-    if 'trial_rating' not in metadata:
+    if ('trial_rating' not in metadata) or (
+            metadata['trial_rating'] == 'No rating given'):
 
         experiment_id = file[-25:-14]
 
-        current_id_cell = metadata_worksheet.find(experiment_id)
-        row_of_metadata = metadata_worksheet.row_values(current_id_cell.row)
-        row_of_keys = metadata_worksheet.row_values(1)
-        gdrive_metadata_dict = dict(zip(row_of_keys, row_of_metadata))
-#        print(gdrive_metadata_dict)
+        if int(experiment_id[4:7]) > 1: # can change this to skip older trials
+            current_id_cell = metadata_worksheet.find(experiment_id)
+            row_of_metadata = metadata_worksheet.row_values(
+                    current_id_cell.row)
+            row_of_keys = metadata_worksheet.row_values(1)
+            gdrive_metadata_dict = dict(zip(row_of_keys, row_of_metadata))
+    #        print(gdrive_metadata_dict)
 
-        try:
-            metadata['trial_rating'] = gdrive_metadata_dict['Trial rating']
-            print(metadata['trial_rating'])
-            with open(file, 'w') as output_file:
-                yaml.dump(metadata, output_file,  # create file
-                          explicit_start=True, default_flow_style=False)
-        except KeyError:
-            print("didn't find a rating value for ", experiment_id)
+            try:
+                metadata['trial_rating'] = gdrive_metadata_dict['Trial rating']
+                print(metadata['trial_rating'])
+                with open(file, 'w') as output_file:
+                    yaml.dump(metadata, output_file,  # create file
+                              explicit_start=True, default_flow_style=False)
+            except KeyError:
+                print("didn't find a rating value for ", experiment_id)
