210ba501db24f2a29ae818e9503d964d96f72f7a
Adam Nekimken <adam.nekimken@gmail.com>
2019-04-04 13:19:21 -0700

    Wrote script to rerun successful trials
    Makes sure any changes that were made get propagated

--- scripts/rerun_successful_trials.py
+++ scripts/rerun_successful_trials.py
@@ -14,26 +14,66 @@ Created on Thu Apr  4 10:36:26 2019
 import yaml
 import glob
 import pandas as pd
-import ssn_image_analysis_gui_controller as ssn_cont
+import run_queue
 
-controller = ssn_cont.StrainGUIController(headless=True)
 
-# TODO: move file path to config file
-with open('config.yaml', 'r') as config_file:
-    file_paths = yaml.safe_load(config_file)
-analyzed_data_dir = file_paths['analysis_dir'] + 'AnalyzedData/'
+def add_successful_trials_to_queue():
+#    controller = ssn_cont.StrainGUIController(headless=True)
 
-# get all the metadata
-experiment_days = glob.iglob(analyzed_data_dir + '*')
-all_metadata = []
-for day in experiment_days:
-    try:
-        with open(day + '/metadata.yaml') as metadata_file:
-            all_metadata.append(yaml.safe_load(metadata_file))
-    except FileNotFoundError:
-        pass
-metadata_df = pd.DataFrame(all_metadata)
+    with open('config.yaml', 'r') as config_file:
+        file_paths = yaml.safe_load(config_file)
+    analyzed_data_dir = file_paths['analysis_dir'] + 'AnalyzedData/'
 
-# filter out trials we don't want
-trials_with_strain_calc = metadata_df.loc[
-        metadata_df['analysis_status'] == 'Strain calculated']
+    # get all the metadata
+    experiment_days = glob.iglob(analyzed_data_dir + '*')
+    all_metadata = []
+    for day in experiment_days:
+        try:
+            with open(day + '/metadata.yaml') as metadata_file:
+                all_metadata.append(yaml.safe_load(metadata_file))
+        except FileNotFoundError:
+            pass
+    metadata_df = pd.DataFrame(all_metadata)
+
+    # filter out trials we don't want
+    trials_with_strain_calc = metadata_df.loc[
+            metadata_df['analysis_status'] == 'Strain calculated']
+
+    the_queue = file_paths['analysis_dir'] + 'analysis_queue.yaml'
+    for index, trial in trials_with_strain_calc.iterrows():
+        print(trial['Experiment_id'])
+
+        batch_param_history_file = (file_paths['analysis_dir'] +
+                                    'AnalyzedData/' +
+                                    trial['Experiment_id'] +
+                                    '/trackpyBatchParamsHistory.yaml')
+
+        with open(batch_param_history_file, 'r') as param_file:
+            entire_history = yaml.load_all(param_file)
+            last_params = None
+            for last_params in entire_history:
+                pass  # pass until we get to most recent
+
+        param_dict = {'experiment_id': trial['Experiment_id'],
+                      'roi': last_params['roi'],
+                      'gaussian_width': last_params['noise_size'],
+                      'particle_z_diameter': last_params['diameter'][0],
+                      'particle_xy_diameter': last_params['diameter'][1],
+                      'brightness_percentile': last_params['percentile'],
+                      'min_particle_mass': last_params['minmass'],
+                      'bottom_slice': last_params['bottom_slice'],
+                      'top_slice': last_params['top_slice'],
+                      'tracking_seach_radius': last_params[
+                              'tracking_seach_radius'],
+                      'last_timepoint': last_params['last_timepoint']}
+        if 'notes' in last_params:
+            param_dict['notes'] = last_params['notes']
+
+        with open(the_queue, 'a') as output_file:
+                yaml.dump(param_dict, output_file, explicit_start=True)
+
+
+if __name__ == '__main__':
+    # execute as script
+    add_successful_trials_to_queue()
+    run_queue.run_queue()
