8595e166ed017e734ba741bfc9fab1a038b173dd
Adam Nekimken <adam.nekimken@gmail.com>
2019-04-04 13:19:21 -0700

    Wrote script to rerun successful trials
    Makes sure any changes that were made get propagated

--- scripts/run_queue.py
+++ scripts/run_queue.py
@@ -11,45 +11,54 @@ import os
 import time
 import ssn_image_analysis_gui_controller as ssn_cont
 
-with open('config.yaml', 'r') as config_file:
-    file_paths = yaml.safe_load(config_file)
-queue_location = file_paths['analysis_dir']
-the_queue = queue_location + 'analysis_queue.yaml'
-controller = ssn_cont.StrainGUIController(headless=True)
 
-with open(the_queue, 'r') as queue_file:
-            entire_queue = yaml.load_all(queue_file)
-            queue_length = len(list(entire_queue))
+def run_queue():
+    with open('config.yaml', 'r') as config_file:
+        file_paths = yaml.safe_load(config_file)
+    queue_location = file_paths['analysis_dir']
+    the_queue = queue_location + 'analysis_queue.yaml'
+    controller = ssn_cont.StrainGUIController(headless=True)
 
-print('Running queue with', queue_length, 'items.')
-error_list = []
-queue_start_time = time.time()
-while True:  # queue_length > 0:
-    queue_file_size = os.stat(the_queue).st_size
-    if queue_file_size > 0:
-        try:
-            controller.run_queue_item(queue_location)
-        except AttributeError as error:
-            if error.args[0] == "'DataFrame' object has no attribute 'dtype'":
-                print('dataframe error')
-                error_list.append((controller.trial.experiment_id, error))
-                # Remove first trial in queue, since we're done with it
-                with open(the_queue, 'r') as queue_file:
-                    old_queue = yaml.load_all(queue_file)
-                    new_queue = [
-                            item for item in old_queue
-                            if item['experiment_id'] !=
-                            controller.trial.experiment_id]
+    with open(the_queue, 'r') as queue_file:
+                entire_queue = yaml.load_all(queue_file)
+                queue_length = len(list(entire_queue))
 
-                with open(the_queue, 'w') as queue_file:
-                    yaml.dump_all(new_queue, queue_file, explicit_start=True)
-                pass
-        # update queue length variable
-        with open(the_queue, 'r') as queue_file:
-            entire_queue = yaml.load_all(queue_file)
-            queue_length = len(list(entire_queue))
-    else:
-        print('Finished queue in',
-              str(round(time.time() - queue_start_time)), 'seconds.')
-        print("Errors occurred on", error_list)
-        break
+    print('Running queue with', queue_length, 'items.')
+    error_list = []
+    queue_start_time = time.time()
+    while True:  # queue_length > 0:
+        queue_file_size = os.stat(the_queue).st_size
+        if queue_file_size > 0:
+            try:
+                controller.run_queue_item(queue_location)
+            except AttributeError as error:
+                if error.args[0] == ("'DataFrame' object has "
+                                     "no attribute 'dtype'"):
+                    print('dataframe error')
+                    error_list.append((controller.trial.experiment_id, error))
+                    # Remove first trial in queue, since we're done with it
+                    with open(the_queue, 'r') as queue_file:
+                        old_queue = yaml.load_all(queue_file)
+                        new_queue = [
+                                item for item in old_queue
+                                if item['experiment_id'] !=
+                                controller.trial.experiment_id]
+
+                    with open(the_queue, 'w') as queue_file:
+                        yaml.dump_all(new_queue, queue_file,
+                                      explicit_start=True)
+                    pass
+            # update queue length variable
+            with open(the_queue, 'r') as queue_file:
+                entire_queue = yaml.load_all(queue_file)
+                queue_length = len(list(entire_queue))
+        else:
+            print('Finished queue in',
+                  str(round(time.time() - queue_start_time)), 'seconds.')
+            print("Errors occurred on", error_list)
+            break
+
+
+if __name__ == '__main__':
+    # run as a script
+    run_queue()
